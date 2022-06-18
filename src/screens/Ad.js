a44d166b244c823a87bf555f959a4215e68637e1
Sungho Moon <moon7959@kakao.com>
2022-06-18 22:40:38 +0900

    광고 재개

--- src/screens/Ad.js
+++ src/screens/Ad.js
@@ -4,27 +4,6 @@ import WebView from "react-native-webview";
 import { Linking } from "react-native";
 
 export default function Ad() {
-  const colors = useTheme();
-  const kakaoAdHtml = `
-  <head>
-  <meta
-    name="viewport"
-    content="width=device-width, initial-scale=1, maximum-scale=1"
-  />
-</head>
-<body style="background-color: ${colors.colors.background};"><ins
-        class="kakao_ad_area"
-        style="display:none; background-color: ${colors.colors.background};"
-        data-ad-unit="DAN-QdCEnC4cqSQZvKQU"
-        data-ad-width="320"
-        data-ad-height="50"
-      ></ins>
-      <script
-        type="text/javascript"
-        src="https://t1.daumcdn.net/kas/static/ba.min.js"
-        async
-      ></script></body>`;
-
   return (
     <View
       style={{
@@ -36,15 +15,13 @@ export default function Ad() {
       }}
     >
       <WebView
-        source={{ html: kakaoAdHtml }}
+        source={{ uri: "https://jazzy-chimera-4f518f.netlify.app" }}
         onError={(error) => {
           console.warn(error);
         }}
         scrollEnabled={false}
-        allowsLinkPreview={false}
-        pagingEnabled={false}
         onNavigationStateChange={(event) => {
-          if (event.url !== "about:blank") {
+          if (event.url !== "https://jazzy-chimera-4f518f.netlify.app/") {
             Linking.openURL(event.url);
           }
         }}
