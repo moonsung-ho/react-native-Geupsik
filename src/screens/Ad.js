a5a1a1af994f08f55b1a49e45e162c290375f3ad
Sungho Moon <moon7959@kakao.com>
2022-11-03 17:00:10 +0900

    ddd

--- src/screens/Ad.js
+++ src/screens/Ad.js
@@ -1,28 +1,33 @@
 import { AdMobBanner } from "expo-ads-admob";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Platform } from "react-native";
 import * as Device from "expo-device";
 import { KEYS, useAsyncStorage } from "../hooks/asyncStorage";
+import * as FacebookAds from "expo-ads-facebook";
 
-const testID = "ca-app-pub-3940256099942544/6300978111";
-const iOSProductionID = "ca-app-pub-7245930610023842/4484950317";
-const androidProductionID = "ca-app-pub-7245930610023842/2837359556";
+// const testID = "ca-app-pub-3940256099942544/6300978111";
+// const iOSProductionID = "ca-app-pub-7245930610023842/4484950317";
+// const androidProductionID = "ca-app-pub-7245930610023842/2837359556";
 
-const adBannerUnitId =
-  Platform.OS === "android"
-    ? Device.isDevice
-      ? androidProductionID
-      : testID
-    : Device.isDevice
-    ? iOSProductionID
-    : testID;
+// const adBannerUnitId =
+//   Platform.OS === "android"
+//     ? Device.isDevice
+//       ? androidProductionID
+//       : testID
+//     : Device.isDevice
+//     ? iOSProductionID
+//     : testID;
+const iOSID = "490492783097602_491489829664564";
+const androidID = "490492783097602_490522789761268";
+
+//const adBannerUnitId = Platform.OS === "android" ? androidID : iOSID;
 
 export default function Ad() {
-  const trackingPermissionAS = useAsyncStorage(KEYS.TRACKINGPERMISSION);
+  //const trackingPermissionAS = useAsyncStorage(KEYS.TRACKINGPERMISSION);
 
   return (
     <View>
-      <AdMobBanner
+      {/* <AdMobBanner
         bannerSize="banner"
         adUnitID={adBannerUnitId} // Test ID, Replace with your-admob-unit-id
         servePersonalizedAds={
@@ -31,6 +36,12 @@ export default function Ad() {
         onDidFailToReceiveAdWithError={(err) => {
           console.warn(err);
         }}
+      /> */}
+      <FacebookAds.BannerAd
+        placementId="490492783097602_491489829664564"
+        type="standard"
+        onPress={() => console.warn("click")}
+        onError={(e) => console.warn(e.timeStamp)}
       />
     </View>
   );
